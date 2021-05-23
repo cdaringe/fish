d85aa13282e88faf4ddf4f379e0dab67f2b69d86
cdaringe <cdaringe@gmail.com>
2021-05-23 00:28:27 -0700

    feat: finish fish & hook wander + catch

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,7 @@ const config: Phaser.Types.Core.GameConfig = {
   physics: {
     default: "arcade",
     arcade: {
+      debug: true,
       gravity: { y: 0 },
     },
   },
--- src/scenes/OceanScene.ts
+++ src/scenes/OceanScene.ts
@@ -3,72 +3,125 @@ import Phaser from "phaser";
 
 export default class OceanScene extends Phaser.Scene {
   public fishies: Phaser.Types.Physics.Arcade.ImageWithDynamicBody[] = [];
+  public hookedFish: Phaser.Types.Physics.Arcade.ImageWithDynamicBody | null =
+    null;
+  private _hook: Phaser.Types.Physics.Arcade.ImageWithDynamicBody | null = null;
+  private hookup: Phaser.Physics.Arcade.Group | null = null;
+
   constructor() {
     super("ocean");
   }
 
+  get hook() {
+    if (!this._hook) throw new Error("_hook uninitialized");
+    return this._hook;
+  }
+
   preload() {
-    this.load.image("fishy_b", "public/fishy_b.svg");
+    this.load.image("fishy_b", "public/fishy_a.svg");
     this.load.image("bubble", "public/bubbleo.svg");
+    this.load.image("hook", "public/hook.svg");
   }
 
   create() {
-    const particles = this.add.particles("bubble");
-    const emitters = [
-      particles.createEmitter({
-        active: true,
-        scale: { start: 0.5, end: 0 },
-        frequency: 2000,
-        blendMode: "ADD",
-        gravityY: -200,
-      }),
-      particles.createEmitter({
-        active: true,
-        scale: { start: 0.3, end: 0 },
-        frequency: 1000,
-        blendMode: "ADD",
-        gravityY: -350,
-      }),
-    ];
+    this.fishies = [...Array(5)].map((_, i) => {
+      const particles = this.add.particles("bubble");
+      const emitters = [
+        particles.createEmitter({
+          active: true,
+          scale: { start: 0.5, end: 0 },
+          frequency: 2000,
+          blendMode: "ADD",
+          gravityY: -200,
+        }),
+        particles.createEmitter({
+          active: true,
+          scale: { start: 0.3, end: 0 },
+          frequency: 1000,
+          blendMode: "ADD",
+          gravityY: -350,
+        }),
+      ];
 
-    const fish = this.physics.add.image(400, 100, "fishy_b");
-    fish.setBounce(0.35, 0.35);
-    fish.setCollideWorldBounds(true);
-    emitters.forEach((em) => em.startFollow(fish));
-    fish.setVelocity(Math.random() * 100 - 50, Math.random() * 100 - 50);
-    fish.body.setMaxVelocity(150, 150);
-    this.fishies.push(fish);
+      const fish = this.physics.add.image(50 * i, 50 * i, "fishy_b");
+      fish.setBounce(0.35, 0.35);
+      fish.setCollideWorldBounds(true);
+      emitters.forEach((em) => {
+        em.stop();
+        setTimeout(() => {
+          em.start();
+        }, Math.random() * 2_000);
+        em.startFollow(fish);
+      });
+      fish.setVelocity(Math.random() * 100 - 50, Math.random() * 100 - 50);
+      fish.body.setMaxVelocity(150, 150);
+      fish.setBounce(1, 1);
+      (fish as any).emitters = emitters;
+      return fish;
+    });
+
+    const hook = this.physics.add.image(100, 100, "hook");
+    hook.setScale(0.04);
+    hook.setSize(200, 200);
+    hook.setOffset(-100, 1200);
+    hook.setCollideWorldBounds();
+    hook.setVelocity(Math.random() * 500 - 50, Math.random() * 500 - 50);
+    this._hook = hook;
+
+    this.fishies.forEach((fish) => {
+      this.physics.add.collider(fish, hook, () => {
+        if (this.hookedFish) return;
+        this.hookedFish = fish;
+        fish.body.setEnable(false);
+        this.hookup = this.physics.add.group([fish, hook]);
+      });
+    });
   }
   update() {
-    for (const fish of this.fishies) {
-      const isOnCeiling = fish.body.onCeiling();
-      if (isOnCeiling || fish.body.onFloor()) {
-        fish.body.acceleration.y *= -1;
-        // fish.body.velocity.y *= -1;
-        // if (fish.body.position.y === 0) fish.body.position.y = 1;
-        // else {
-        //   fish.body.position.y -= 1;
-        // }
-      } else if (fish.body.onWall()) {
-        fish.body.acceleration.x *= -1;
-        // fish.body.velocity.x *= -1;
-        // if (fish.body.position.x === 0) fish.body.position.x = 1;
-        // else {
-        //   fish.body.position.x -= 1;
-        // }
-      }
-      // } else {
-      const dir = Math.random() >= 0.5 ? 1 : -1;
-      const adjustX = Math.random() <= 0.5;
-      const rand = Math.random();
-      const { x, y } = fish.body.acceleration;
-      if (adjustX) {
-        fish.body.setAccelerationX(x + dir * rand * 10);
+    const sprites = [
+      ...this.fishies.map((fish) => [fish, "fish"] as const),
+      [this.hook, "hook"] as const,
+    ];
+    for (const [sprite, type] of sprites) {
+      if (sprite === this.hookedFish || !sprite.body) {
+        continue;
       } else {
-        fish.body.setAccelerationY(y + dir * rand * 10);
+        const hookedFish = this.hookedFish;
+        if (hookedFish) {
+          hookedFish.setVelocity(0, 0);
+          this.hookup?.setVelocityY(-400);
+          const { x, y } = this.hook.body.position;
+          hookedFish.setPosition(x, y);
+          if (y <= 0) {
+            this.fishies = this.fishies.filter((fish) => fish !== hookedFish);
+            (
+              (hookedFish as any)
+                .emitters as Phaser.GameObjects.Particles.ParticleEmitter[]
+            ).forEach((emitter) => emitter.remove());
+            hookedFish.destroy();
+            this.hookup?.clear();
+            this.hook.setCollideWorldBounds();
+            this.hookup = null;
+            this.hookedFish = null;
+            this.hook.setVelocityY(100);
+          }
+        }
       }
-      fish.flipX = fish.body.velocity.x < 0;
-      // }
+      const isOnCeiling = sprite.body.onCeiling();
+      if (isOnCeiling || sprite.body.onFloor()) {
+        sprite.body.acceleration.y *= -1;
+      } else if (sprite.body.onWall()) {
+        sprite.body.acceleration.x *= -1;
+      }
+      const dir = Math.random() >= 0.5 ? 1 : -1;
+      const isRandomizingX = Math.random() <= 0.5;
+      const rand = Math.random();
+      const { x, y } = sprite.body.acceleration;
+      if (isRandomizingX) {
+        sprite.body.setAccelerationX(x + dir * rand * 10);
+      } else {
+        sprite.body.setAccelerationY(y + dir * rand * 10);
+      }
     }
   }
 }
